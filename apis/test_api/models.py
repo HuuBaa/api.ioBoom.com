7e26f0a4977020ccd5ecdd5686a6be8ca5194209
HuuBaa <742790905@qq.com>
2018-07-19 19:41:15 +0800

    7-19

--- apis/test_api/models.py
+++ apis/test_api/models.py
@@ -6,7 +6,7 @@ class TestApiModel(models.Model):
     name=models.CharField(max_length=100,verbose_name="名字",help_text="名字")
     numbers=models.BigIntegerField(verbose_name="数量",help_text="数量")
     time=models.DateTimeField(verbose_name="时间",help_text="时间",default=datetime.now)
-    content=models.TextField(verbose_name="内容啊",help_text="内容啊")
+    content=models.TextField(verbose_name="内容",help_text="内容")
 
     def __str__(self):
         return "测试api(id:{0})".format(self.id)
